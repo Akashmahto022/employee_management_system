3a60114e3cb56608115c625e9943f66fd9665de5
Akashmahto022 <akashmahto1169@gmail.com>
2024-11-07 10:24:59 +0530

    add filteration to find an employee

--- office_management_sys/emp_app/views.py
+++ office_management_sys/emp_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Employee, Department, Role
 from django.shortcuts import redirect
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -25,16 +26,36 @@ def add_emp(request):
         new_emp = Employee(first_name = first_name, last_name = last_name, dept_id = dept, salary= salary, bonus = bonus, role_id= role, phone=phone, hire_date=hire_date)
         new_emp.save()
         return redirect('all_emp')
-    elif request.method == 'GET':
-        return render(request, 'add_emp.html')
-    
     else:
         return render(request, 'add_emp.html')
 
 
-def remove_emp(request):
-    return render(request, 'remove_emp.html')
+def remove_emp(request, emp_id = 0):
+    if emp_id:
+        try:
+            emp_to_be_delete = Employee.objects.get(id=emp_id)
+            emp_to_be_delete.delete()
+        except Exception as e:
+            raise e
+    emp = Employee.objects.all()
+    return render(request, 'remove_emp.html', {'emp':emp})
 
 
 def filter_emp(request):
-    return render(request, 'filter_emp.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        dept = request.POST['dept']
+        role = request.POST['role']
+        emps = Employee.objects.all()
+        
+        if name:
+            emps.filter(Q(first_name__icontains = name) | Q(last_name__icontains = name))
+        if dept:
+            emps = emps.filter(dept__name__icontains = dept)
+        if role:
+            emps = emps.filter(role__name__icontains = role)
+            
+        return render(request, 'all_emp.html', {'emps': emps})
+    
+    else:
+        return render(request, 'filter_emp.html')
